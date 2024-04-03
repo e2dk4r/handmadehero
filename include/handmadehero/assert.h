f988783661894cf50e494e80d4faebd5c5f63328
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2024-04-03 22:34:14 +0300

    src: assert: remove assertions at release

--- include/handmadehero/assert.h
+++ include/handmadehero/assert.h
@@ -1,11 +1,20 @@
 #ifndef HANDMADEHERO_ASSERT_H
 #define HANDMADEHERO_ASSERT_H
 
+#if HANDMADEHERO_DEBUG
+
 #define assert(expression)                                                     \
   if (!(expression)) {                                                         \
     __builtin_trap();                                                          \
   }
 
 #define InvalidCodePath __builtin_trap()
+
+#else
+
+#define assert()
+#define InvalidCodePath
+
+#endif
 
 #endif /* HANDMADEHERO_ASSERT_H */
