cc60c1b079d8a029def699c5cfb2d6d72bcdaed0
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2024-04-13 19:43:49 +0300

    ci: fix gcc conversion issue
    ../src/handmadehero_entity.c: In function ‘EntityAddFlag’:
    ../src/handmadehero_entity.c:13:20: error: conversion from ‘unsigned int’ to ‘unsigned char:4’ may change value [-Werror=conversion]
       13 |   entity->flags |= (flag & 0xffff);
          |                    ^
    ../src/handmadehero_entity.c: In function ‘EntityClearFlag’:
    ../src/handmadehero_entity.c:19:20: error: conversion from ‘unsigned int’ to ‘unsigned char:4’ may change value [-Werror=conversion]
       19 |   entity->flags &= ~flag;

--- src/handmadehero_entity.c
+++ src/handmadehero_entity.c
@@ -4,17 +4,17 @@
 inline u8
 EntityIsFlagSet(struct entity *entity, enum entity_flag flag)
 {
-  return entity->flags & flag;
+  return (u8)(entity->flags & flag);
 }
 
 inline void
 EntityAddFlag(struct entity *entity, enum entity_flag flag)
 {
-  entity->flags |= flag;
+  entity->flags |= (flag & 0xf);
 }
 
 inline void
 EntityClearFlag(struct entity *entity, enum entity_flag flag)
 {
-  entity->flags &= ~flag;
+  entity->flags &= (~flag & 0xf);
 }
