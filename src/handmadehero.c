82f329f8fb882a4c88ef512643820d53a2b55a5d
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2024-05-15 16:47:39 +0300

    src: game: fix render height of entities

--- src/handmadehero.c
+++ src/handmadehero.c
@@ -1205,8 +1205,9 @@ GameUpdateAndRender(struct game_memory *memory, struct game_input *input, struct
 
       HitPoints(renderGroup, entity);
 
-      f32 heroHeight = 2.5f * 1.2f;
-      BitmapWithAlpha(renderGroup, &state->textureShadow, v3(0.0f, 0.0f, 0.0f), 0.25f, shadowAlpha);
+      f32 heroHeightC = 2.5f;
+      f32 heroHeight = heroHeightC * 1.2f;
+      BitmapWithAlpha(renderGroup, &state->textureShadow, v3(0.0f, 0.0f, 0.0f), heroHeightC * 1.0f, shadowAlpha);
       Bitmap(renderGroup, &bitmap->torso, v3(0.0f, 0.0f, 0.0f), heroHeight);
       Bitmap(renderGroup, &bitmap->cape, v3(0.0f, 0.0f, 0.0f), heroHeight);
       Bitmap(renderGroup, &bitmap->head, v3(0.0f, 0.0f, 0.0f), heroHeight);
@@ -1253,8 +1254,8 @@ GameUpdateAndRender(struct game_memory *memory, struct game_input *input, struct
       f32 bobSin = Sin(2.0f * entity->tBob);
       f32 shadowAlpha = (0.5f * 1.0f) - (0.2f * bobSin);
 
-      BitmapWithAlpha(renderGroup, &state->textureShadow, v3(0.0f, 0.0f, 0.0f), shadowAlpha, 0.5f);
-      Bitmap(renderGroup, &bitmap->head, v3(0.0f, 0.0f, 0.25f * bobSin), 0.5f);
+      BitmapWithAlpha(renderGroup, &state->textureShadow, v3(0.0f, 0.0f, 0.0f), 2.5f, shadowAlpha);
+      Bitmap(renderGroup, &bitmap->head, v3(0.0f, 0.0f, 0.25f * bobSin), 2.5f);
     }
 
     else if (entity->type & ENTITY_TYPE_MONSTER) {
@@ -1263,8 +1264,8 @@ GameUpdateAndRender(struct game_memory *memory, struct game_input *input, struct
       f32 alpha = 1.0f;
 
       HitPoints(renderGroup, entity);
-      BitmapWithAlpha(renderGroup, &state->textureShadow, v3(0.0f, 0.0f, 0.0f), 0.5f, alpha);
-      Bitmap(renderGroup, &bitmap->torso, v3(0.0f, 0.0f, 0.0f), 0.5f);
+      BitmapWithAlpha(renderGroup, &state->textureShadow, v3(0.0f, 0.0f, 0.0f), 4.5f, alpha);
+      Bitmap(renderGroup, &bitmap->torso, v3(0.0f, 0.0f, 0.0f), 4.5f);
     }
 
     else if (entity->type & ENTITY_TYPE_SWORD) {
