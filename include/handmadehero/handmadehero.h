e2dk4r/handmadehero

commit ee75eb3c433405e0ce319cd8dccaab1d2e40057b

src: game: fix ground buffer not aligned to 4 pixels
Add "lowPriorityQueue" to "transient_state" struct, causes ground buffer
bitmap allocations to be not aligned.

include/handmadehero/handmadehero.h
@@ -97,7 +97,7 @@ struct transient_state {
   struct ground_buffer *groundBuffers;
 
   struct platform_work_queue *highPriorityQueue;
-  struct platform_work_queue *lowPriorityQueue;
+  // struct platform_work_queue *lowPriorityQueue;
 
   u32 envMapWidth;
   u32 envMapHeight;

src/handmadehero.c
@@ -944,7 +944,7 @@ GameUpdateAndRender(struct game_memory *memory, struct game_input *input, struct
     MemoryArenaInit(&transientState->transientArena, data, size);
 
     transientState->highPriorityQueue = memory->highPriorityQueue;
-    transientState->lowPriorityQueue = memory->lowPriorityQueue;
+    // transientState->lowPriorityQueue = memory->lowPriorityQueue;
 
     /* cache composited ground drawing */
     // TODO(e2dk4r): pick a real value here

commit ca2850843f32d87ddd79df92f01ae1b4ca58e2c8

src: memory_arena: add allocating memory with alignment
Instead of relaying of luck, correctly allocate memory for operations
that requires aligned memory.

Fixes: ee75eb3c433405e0ce319cd8dccaab1d2e40057b

include/handmadehero/handmadehero.h
@@ -97,7 +97,7 @@ struct transient_state {
   struct ground_buffer *groundBuffers;
 
   struct platform_work_queue *highPriorityQueue;
-  // struct platform_work_queue *lowPriorityQueue;
+  struct platform_work_queue *lowPriorityQueue;
 
   u32 envMapWidth;
   u32 envMapHeight;

include/handmadehero/memory_arena.h
@@ -33,6 +33,8 @@ void
 MemoryArenaInit(struct memory_arena *mem, void *data, memory_arena_size_t size);
 void *
 MemoryArenaPush(struct memory_arena *mem, memory_arena_size_t size);
+void *
+MemoryArenaPushAlignment(struct memory_arena *mem, memory_arena_size_t size, memory_arena_size_t alignment);
 
 void *
 MemoryChunkPush(struct memory_chunk *chunk);

src/handmadehero.c
@@ -442,7 +442,7 @@ MakeEmptyBitmap(struct memory_arena *arena, u32 width, u32 height)
       .width = width,
       .height = height,
       .stride = (i32)width * BITMAP_BYTES_PER_PIXEL,
-      .memory = MemoryArenaPush(arena, totalBitmapSize),
+      .memory = MemoryArenaPushAlignment(arena, totalBitmapSize, 16),
       .alignPercentage = v2(0.5f, 0.5f),
       .widthOverHeight = (f32)width / (f32)height,
   };
@@ -944,7 +944,7 @@ GameUpdateAndRender(struct game_memory *memory, struct game_input *input, struct
     MemoryArenaInit(&transientState->transientArena, data, size);
 
     transientState->highPriorityQueue = memory->highPriorityQueue;
-    // transientState->lowPriorityQueue = memory->lowPriorityQueue;
+    transientState->lowPriorityQueue = memory->lowPriorityQueue;
 
     /* cache composited ground drawing */
     // TODO(e2dk4r): pick a real value here

src/handmadehero_linux.c
@@ -1322,7 +1322,7 @@ main(int argc, char *argv[])
     goto wl_exit;
   }
 
-  state.backbuffer.memory = MemoryArenaPush(&state.wayland_arena, (size_t)backbuffer_size);
+  state.backbuffer.memory = MemoryArenaPushAlignment(&state.wayland_arena, (size_t)backbuffer_size, 16);
   state.backbuffer.memory =
       mmap(state.backbuffer.memory, (size_t)backbuffer_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
   if (state.backbuffer.memory == MAP_FAILED) {

src/handmadehero_memory_arena.c
@@ -1,4 +1,5 @@
 #include <handmadehero/assert.h>
+#include <handmadehero/math.h>
 #include <handmadehero/memory_arena.h>
 
 inline void
@@ -12,9 +13,20 @@ MemoryArenaInit(struct memory_arena *mem, void *data, memory_arena_size_t size)
 inline void *
 MemoryArenaPush(struct memory_arena *mem, memory_arena_size_t size)
 {
-  assert(mem->used + size <= mem->size && "arena capacity exceeded");
+  return MemoryArenaPushAlignment(mem, size, 4);
+}
 
-  void *data = mem->data + mem->used;
+inline void *
+MemoryArenaPushAlignment(struct memory_arena *mem, memory_arena_size_t size, memory_arena_size_t alignment)
+{
+  assert(1 << FindLeastSignificantBitSet((i32)alignment) == alignment && "alignment must be power of 2");
+  memory_arena_size_t dataPointer = (memory_arena_size_t)mem->data + mem->used;
+  memory_arena_size_t alignmentMask = alignment - 1;
+  memory_arena_size_t alignmentOffset = dataPointer & alignmentMask;
+  size += alignmentOffset;
+
+  assert(mem->used + size <= mem->size && "arena capacity exceeded");
+  void *data = (void *)(dataPointer + alignmentOffset);
   mem->used += size;
 
   return data;