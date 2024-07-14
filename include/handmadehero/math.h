e2dk4r/handmadehero

commit c19e953cd54830f055783e959a2b3a8bb91876f5

src: game: add outputting audio
See: https://docs.pipewire.org/page_tutorial4.html

include/handmadehero/math.h
@@ -7,17 +7,11 @@
 comptime f32 PI32 = 3.14159274101257324f;
 comptime f32 TAU32 = 6.28318530717958647692f;
 
-internal inline f32
-Minimum(f32 a, f32 b)
-{
-  return (a < b) ? a : b;
-}
+// (a < b) ? a : b
+#define Minimum(a, b) ((a < b) ? a : b)
 
-internal inline f32
-Maximum(f32 a, f32 b)
-{
-  return (a > b) ? a : b;
-}
+// (a > b) ? a : b
+#define Maximum(a, b) ((a < b) ? a : b)
 
 internal inline b32
 InRange(f32 min, f32 max, f32 value)

include/handmadehero/asset.h
@@ -10,7 +10,7 @@
 struct audio {
   u32 channelCount;
   u32 sampleCount;
-  u16 *samples[2];
+  i16 *samples[2];
 };
 
 enum asset_state {

include/handmadehero/errors.h
@@ -23,6 +23,8 @@
 #define HANDMADEHERO_ERROR_ALLOCATION 50
 #define HANDMADEHERO_ERROR_SHARED_MEMORY 60
 
-#define HANDMADEHERO_ERROR_THREAD_INIT 70
+#define HANDMADEHERO_ERROR_THREAD_INIT 80
+
+#define HANDMADEHERO_ERROR_PIPEWIRE 90
 
 #endif /* HANDMADEHERO_ERROR_H */

include/handmadehero/handmadehero.h
@@ -73,6 +73,9 @@ struct game_state {
 
   struct bitmap testDiffuse;
   struct bitmap testNormal;
+
+  struct audio testAudio;
+  u32 testAudioSampleIndex;
 };
 
 struct task_with_memory {

include/handmadehero/platform.h
@@ -160,4 +160,14 @@ GameUpdateAndRender(struct game_memory *memory, struct game_input *input, struct
 typedef void (*pfnGameUpdateAndRender)(struct game_memory *memory, struct game_input *input,
                                        struct game_backbuffer *backbuffer);
 
+struct game_audio_buffer {
+  u32 sampleRate;
+  u32 sampleCount;
+  i16 *samples;
+};
+
+void
+GameOutputAudio(struct game_memory *memory, struct game_audio_buffer *buffer);
+typedef void (*pfnGameOutputAudio)(struct game_memory *memory, struct game_audio_buffer *buffer);
+
 #endif /* HANDMADEHERO_PLATFORM_H */

meson.build
@@ -58,6 +58,7 @@ liburing = dependency('liburing')
 vulkan = dependency('vulkan')
 libm = cc.find_library('m')
 libpthread = cc.find_library('pthread')
+libpipewire = dependency('libpipewire-0.3')
 
 subdir('protocol')
 
@@ -118,5 +119,6 @@ executable(
     vulkan,
     libm,
     libpthread,
+    libpipewire,
   ]
 )

src/handmadehero.c
@@ -667,6 +667,26 @@ pfnPlatformWorkQueueAddEntry PlatformWorkQueueAddEntry;
 pfnPlatformWorkQueueCompleteAllWork PlatformWorkQueueCompleteAllWork;
 
 void
+GameOutputAudio(struct game_memory *memory, struct game_audio_buffer *audioBuffer)
+{
+  struct game_state *state = memory->permanentStorage;
+
+  struct audio *audio = &state->testAudio;
+  if (audio->sampleCount == 0)
+    return;
+
+  i16 *sampleOut = audioBuffer->samples;
+  for (u32 sampleIndex = 0; sampleIndex < audioBuffer->sampleCount; sampleIndex++) {
+    i16 sampleValue = audio->samples[0][(state->testAudioSampleIndex + sampleIndex) % audio->sampleCount];
+
+    *sampleOut++ = sampleValue;
+    *sampleOut++ = sampleValue;
+  }
+
+  state->testAudioSampleIndex += audioBuffer->sampleCount;
+}
+
+void
 GameUpdateAndRender(struct game_memory *memory, struct game_input *input, struct game_backbuffer *backbuffer)
 {
 #if HANDMADEHERO_INTERNAL
@@ -691,7 +711,7 @@ GameUpdateAndRender(struct game_memory *memory, struct game_input *input, struct
    * INITIALIZATION
    ****************************************************************/
   if (!state->isInitialized) {
-    LoadWav(memory->PlatformReadEntireFile, "test3/bloop_00.wav");
+    state->testAudio = LoadWav(memory->PlatformReadEntireFile, "test3/music_test.wav");
 
     void *data = memory->permanentStorage + sizeof(*state);
     memory_arena_size_t size = memory->permanentStorageSize - sizeof(*state);

src/handmadehero_asset.c
@@ -486,7 +486,7 @@ LoadWav(pfnPlatformReadEntireFile PlatformReadEntireFile, char *filename)
   assert(header->waveId == WAVE_CHUNKID_WAVE);
 
   struct wave_fmt *fmt = 0;
-  u16 *sampleData = 0;
+  i16 *sampleData = 0;
   u32 sampleDataSize = 0;
   for (struct wave_chunk_iterator iterator = WaveChunkParse(header); IsWaveChunkValid(iterator);
        iterator = WaveChunkNext(iterator)) {
@@ -501,7 +501,7 @@ LoadWav(pfnPlatformReadEntireFile PlatformReadEntireFile, char *filename)
 
     iterator = WaveChunkNext(iterator);
     assert(iterator.chunk->id == WAVE_CHUNKID_DATA && "malformed file");
-    sampleData = (u16 *)WaveChunkData(iterator.chunk);
+    sampleData = (i16 *)WaveChunkData(iterator.chunk);
     sampleDataSize = iterator.chunk->size;
 
     break;
@@ -510,7 +510,7 @@ LoadWav(pfnPlatformReadEntireFile PlatformReadEntireFile, char *filename)
   assert(fmt && sampleData);
 
   result.channelCount = fmt->numChannels;
-  result.sampleCount = sampleDataSize / fmt->numChannels * sizeof(u16);
+  result.sampleCount = sampleDataSize / (fmt->numChannels * sizeof(u16));
   switch (fmt->numChannels) {
   case 1:
     result.samples[0] = sampleData;
@@ -521,13 +521,15 @@ LoadWav(pfnPlatformReadEntireFile PlatformReadEntireFile, char *filename)
     result.samples[0] = sampleData;
     result.samples[1] = sampleData + result.sampleCount;
 
+#if 0
     for (u32 sampleIndex = 0; sampleIndex < result.sampleCount; sampleIndex++) {
-      sampleData[sampleIndex * 2 + 0] = (u16)sampleIndex;
-      sampleData[sampleIndex * 2 + 1] = (u16)sampleIndex;
+      sampleData[sampleIndex * 2 + 0] = (i16)sampleIndex;
+      sampleData[sampleIndex * 2 + 1] = (i16)sampleIndex;
     }
+#endif
 
     for (u32 sampleIndex = 0; sampleIndex < result.sampleCount; sampleIndex++) {
-      u16 source = sampleData[sampleIndex * 2];
+      i16 source = sampleData[sampleIndex * 2];
       sampleData[sampleIndex * 2] = sampleData[sampleIndex];
       sampleData[sampleIndex] = source;
     }

commit 1eaa09836fa7cb8a48c229998fe2ebef21714795

include: math: fix typo on Maximum()
Introduced in c19e953cd54830f055783e959a2b3a8bb91876f5 day-138.

include/handmadehero/math.h
@@ -11,7 +11,7 @@ comptime f32 TAU32 = 6.28318530717958647692f;
 #define Minimum(a, b) ((a < b) ? a : b)
 
 // (a > b) ? a : b
-#define Maximum(a, b) ((a < b) ? a : b)
+#define Maximum(a, b) ((a > b) ? a : b)
 
 internal inline b32
 InRange(f32 min, f32 max, f32 value)